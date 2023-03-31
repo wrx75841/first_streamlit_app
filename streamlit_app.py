91e71c3c9f586a29c3214606c6a9dde42b360cf6
wrx75841 <97839711+wrx75841@users.noreply.github.com>
2023-03-31 22:34:03 +0200

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Breakfast Menu')
 streamlit.text('Breakfast Menu')
 streamlit.text('Breakfast Menu')
