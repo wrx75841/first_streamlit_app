7fdf91c1666baedca867fb15ce5af999bca8f22d
wrx75841 <97839711+wrx75841@users.noreply.github.com>
2023-03-31 22:34:36 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Breakfast Menu')
-streamlit.text('Breakfast Menu')
-streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
